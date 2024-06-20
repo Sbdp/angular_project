42bb25ff64e8e4d0b26c548d60831598a07265ba
Subhodeep Das <dassubhodeep.10@gmail.com>
2024-06-20 13:26:59 +0530

    Initial commit

---
+++ src/app/crew-time-table/crew-time-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-time-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './crew-time-table.component.html',
+  styleUrl: './crew-time-table.component.scss'
+})
+export class CrewTimeTableComponent {
+
+}
